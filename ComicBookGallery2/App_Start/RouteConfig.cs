2793d51d5b0480facd47c28f3d54a20b50b89e0c
destinydiver <destinydiver@gmail.com>
2020-05-22 12:27:09 -0400

    Added comic books controller

--- ComicBookGallery2/App_Start/RouteConfig.cs
+++ ComicBookGallery2/App_Start/RouteConfig.cs
@@ -17,7 +17,7 @@ namespace ComicBookGallery2
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-                // test
+                
             );
         }
     }

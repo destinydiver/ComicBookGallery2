c7e28cc3a73edadb9b02f72952c03c720993ded3
destinydiver <destinydiver@gmail.com>
2020-05-27 18:27:18 -0400

    Added ComicBooks List View

--- ComicBookGallery2/Data/ComicBookRepository.cs
+++ ComicBookGallery2/Data/ComicBookRepository.cs
@@ -58,6 +58,11 @@ namespace ComicBookGallery2.Data
                 Favorite = false
             }
         };
+
+        public ComicBook[] GetComicBooks()
+        {
+            return _comicBooks;
+        }
         
         public ComicBook GetComicBook(int id)
         {
